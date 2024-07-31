6d93d5c329525a1156c7b67dbd15f982991e623a
huly <qq6308qq@gmail.com>
2024-07-31 22:00:03 +0800

    稍微修改一下 更合適的寫法及排版

--- app/main.py
+++ app/main.py
@@ -5,15 +5,18 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
-    conn, addr = server_socket.accept()
+    conn, _addr = server_socket.accept()
 
-    content = conn.recv(1024)
-    content_decoded = content.decode()
-    is_pure_path = content_decoded[content_decoded.find("/") + 1] == " "
+    content = conn.recv(1024).decode()
+
+    is_pure_path = content[content.find("/") + 1] == " "
+
     if is_pure_path:
         conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     else:
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    conn.close()
 
 
 if __name__ == "__main__":
