99bc5c9421a648e8537aecfb43b04e1a10b852a8
huly <qq6308qq@gmail.com>
2024-07-31 21:55:58 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,13 @@ def main():
 
     conn, addr = server_socket.accept()
 
-    conn.recv(1024)
-
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    content = conn.recv(1024)
+    content_decoded = content.decode()
+    is_pure_path = content_decoded[content_decoded.find("/") + 1] == " "
+    if is_pure_path:
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
