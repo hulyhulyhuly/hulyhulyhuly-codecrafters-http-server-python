27d8164795f499ddfb17a9f7b9185664d873d05c
huly <qq6308qq@gmail.com>
2024-08-01 11:34:41 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,33 @@
-# Uncomment this to pass the first stage
-import socket
 import re
+import socket
+import threading
 
 
 class ResponseStatus:
     OK_200 = b"HTTP/1.1 200 OK\r\n\r\n"
-    OK_200_with_body = (
-        lambda data: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode()
-    )
-    OK_200_with_user_agent = (
-        lambda user_agent: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
-    )
+
+    def OK_200_with_body(data: str) -> bytes:
+        response = (
+            f"HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain\r\n"
+            f"Content-Length: {len(data)}\r\n\r\n"
+            f"{data}"
+        )
+        return response.encode()
+
+    def OK_200_with_user_agent(user_agent: str) -> bytes:
+        response = (
+            f"HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain\r\n"
+            f"Content-Length: {len(user_agent)}\r\n\r\n"
+            f"{user_agent}"
+        )
+        return response.encode()
+
     NOT_FOUND_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 
-def main() -> None:
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.listen()
-    conn, _addr = server_socket.accept()
-
+def server_thread(conn, _addr):
     content = conn.recv(1024).decode()
 
     """
@@ -26,46 +35,44 @@ def main() -> None:
     [1] -> Path
     [2] -> Http's Version
     """
-    # print(content)
     contents = content.split(" ")
-    path = content.split(" ")[1] or None
+    path = contents[1] or None
 
-    rule_user_agent = "User-Agent: (.*)\r\n"
+    rule_user_agent = "User-Agent: (.*?)\r\n"
     result_user_agent = re.search(rule_user_agent, content)
 
     rule_echo = "/echo/(.*)"
     result_echo = re.search(rule_echo, path)
 
-    if path == "/" and not result_user_agent:
-        conn.sendall(ResponseStatus.OK_200)
-    elif result_user_agent:
-        res = ResponseStatus.OK_200_with_user_agent(result_user_agent.group(1))
-        conn.sendall(res)
+    if path == "/":
+        if result_user_agent:
+            res = ResponseStatus.OK_200_with_user_agent(result_user_agent.group(1))
+            conn.sendall(res)
+        else:
+            conn.sendall(ResponseStatus.OK_200)
     elif result_echo:
         res = ResponseStatus.OK_200_with_body(result_echo.group(1))
         conn.sendall(res)
     else:
         conn.sendall(ResponseStatus.NOT_FOUND_404)
 
+    conn.close()
 
-# HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
 
+def main() -> None:
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen()
 
-def re_demo() -> None:
-    rules = [
-        "/",
-        "/echo/(.*)",
-    ]
-
-    path = "/echo/abc/s"
-
-    # for _, rule in enumerate(rules):
-    #     result =
-
-    #     if result is not None:
-    #         print(result.group(1))
+    try:
+        while True:
+            conn, _addr = server_socket.accept()
+            threading.Thread(target=server_thread, args=(conn, _addr)).start()
+    except KeyboardInterrupt:
+        print("Server is shutting down...")
+    finally:
+        server_socket.close()
+        print("Server has been shut down.")
 
 
 if __name__ == "__main__":
     main()
-    # re_demo()
