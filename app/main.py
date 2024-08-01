46f2906a08523b56f3568731dddae595475c8c88
huly <qq6308qq@gmail.com>
2024-08-01 08:30:02 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ class ResponseStatus:
     OK_200_with_body = (
         lambda data: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode()
     )
+    OK_200_with_user_agent = (
+        lambda user_agent: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+    )
     NOT_FOUND_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 
@@ -24,16 +27,22 @@ def main() -> None:
     [2] -> Http's Version
     """
     # print(content)
+    contents = content.split(" ")
     path = content.split(" ")[1] or None
 
-    rule = "/echo/(.*)"
+    rule_user_agent = "User-Agent: (.*)\r\n"
+    result_user_agent = re.search(rule_user_agent, content)
 
-    result = re.search(rule, path, re.I)
+    rule_echo = "/echo/(.*)"
+    result_echo = re.search(rule_echo, path)
 
-    if path == "/":
+    if path == "/" and not result_user_agent:
         conn.sendall(ResponseStatus.OK_200)
-    elif result:
-        res = ResponseStatus.OK_200_with_body(result.group(1))
+    elif result_user_agent:
+        res = ResponseStatus.OK_200_with_user_agent(result_user_agent.group(1))
+        conn.sendall(res)
+    elif result_echo:
+        res = ResponseStatus.OK_200_with_body(result_echo.group(1))
         conn.sendall(res)
     else:
         conn.sendall(ResponseStatus.NOT_FOUND_404)
